7daa3b812e17da00d6f301062d6e2a021a9803f1
Ronika <rconner92@gmail.com>
2019-08-03 00:49:26 -0400

    First commit

---
+++ Assets/Scripts/BGScroller.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGScroller : MonoBehaviour 
+{
+
+    public float scrollSpeed;
+    public float tileSizeZ;
+    public bool beenset;
+    private Vector3 startPosition;
+    public GameController gameController;
+
+    void Start () 
+    {
+        
+        startPosition = transform.position;
+    }
+
+    void Update ()
+    {
+        if (gameController.score >= 300)
+        {
+            scrollSpeed = scrollSpeed + -2;
+        }
+
+     
+        float newPosition = Mathf.Repeat (Time.time * scrollSpeed, tileSizeZ);
+        transform.position = startPosition + Vector3.forward * newPosition;
+        
+    }
+}
+
---
+++ Assets/Scripts/ParticleControl.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleControl : MonoBehaviour 
+{
+
+    
+    public GameController gameController;
+    public ParticleSystem ps;
+    public float defaultVelocity;
+    public float VelocityMultiplyer;
+    void Start () 
+    {
+        ps = GetComponent<ParticleSystem>();
+        
+    }
+
+    void Update ()
+    {
+        ParticleSystem.MainModule main = ps.main;
+        if (gameController.score >= 300)
+        {
+             main.simulationSpeed = defaultVelocity * VelocityMultiplyer;
+        }
+
+        
+    }
+}
+
+
+ /* public void ParticleSpeed (ParticleSystem ps, float defaultVelocity, float VelocityMultiplyer){
+ 
+     ParticleSystem.MainModule main = ps.main;
+ 
+     main.simulationSpeed = defaultVelocity * VelocityMultiplyer;
+ 
+     Debug.Log ("Particle simSpeed : " + main.simulationSpeed);
+ 
+ }*/
